9aa0f7b6a968524700caef0f19a7300b20af02d0
Eloy Dominguez <eloydominguez3@gmail.com>
2025-10-31 17:00:39 +0100

    modificacion de frontend

--- Frontend/JS/landing_page.js
+++ Frontend/JS/landing_page.js
@@ -3,14 +3,14 @@ function toggleForm() {
 };
 
 document.getElementById('registro').addEventListener('submit', function(event) {
-  event.preventDefault(); // Evita el envío tradicional del formulario
+  event.preventDefault(); 
 
-  const form = event.target;
+  const form = event.target;  
   const formData = new FormData(form);
 
   const data = Object.fromEntries(formData.entries());
 
-  fetch('/login', {
+  fetch('http://localhost:5000/login', {
     method: 'POST',
     headers: {
       'Content-Type': 'application/json'
@@ -23,6 +23,35 @@ document.getElementById('registro').addEventListener('submit', function(event) {
   })
   .then(result => {
     console.log('Respuesta del servidor:', result);
+  })
+  .catch(error => {
+    console.error('Error en la petición:', error);
+  });
+});
+
+document.getElementById('inicio').addEventListener('submit', function(event) {
+    event.preventDefault()
+
+    const form = event.target;
+    const formData = new FormData(form);
+
+  // Convertimos FormData a un objeto plano
+  const data = Object.fromEntries(formData.entries());
+
+  fetch('http://localhost:5000/login', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify(data)
+  })
+  .then(response => {
+    if (!response.ok) throw new Error('Error en la respuesta');
+    return response.json();
+  })
+  .then(result => {
+    console.log('Respuesta del servidor:', result);
+    // Aquí puedes redirigir o mostrar un mensaje
   })
   .catch(error => {
     console.error('Error en la petición:', error);
